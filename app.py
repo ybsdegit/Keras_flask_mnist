7ff2a83dc7be5956cb2c5f066cfdc59708288931
ybsdegit <ybsdeyx@163.com>
2019-12-16 22:45:39 +0800

    最新模型，测试集成功率99%，自己手写的成功率80%

--- app.py
+++ app.py
@@ -13,11 +13,6 @@ import re
 import base64
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
-import sys
-import os
-# sys.path.append(os.path.abspath("./model"))
-# from load import *
-
 app = Flask(__name__)
 
 model_file = './model/model.h5'
@@ -31,28 +26,16 @@ def index():
 @app.route('/predict/', methods=['Get', 'POST'])
 def preditc():
     parseImage(request.get_data())
-
     img = img_to_array(load_img('output.png', target_size=(28, 28), color_mode="grayscale")) / 255.
     img = np.expand_dims(img, axis=0)
-    print(img.shape) #(1, 28, 28, 1)
-   
-    # out = model.predict(img)
-    # print(out)
-    # print(np.argmax(out, axis=1))
-    # response = np.array_str(np.argmax(out, axis=1))
     response = model.predict_classes(img)[0]
-
     return str(response)
 
-
 def parseImage(imgData):
-    # parse canvas bytes and save as output.png
     imgStr = re.search(b'base64,(.*)', imgData).group(1)
-    print(imgStr)
     with open('./output.png', 'wb') as output:
         output.write(base64.decodebytes(imgStr))
 
 
 if __name__ == '__main__':
-    # app.debug = True
-    app.run()
+    app.run(host="0.0.0.0", port=3335)
--- model/train.py
+++ model/train.py
@@ -5,21 +5,18 @@
 # @File    : train.py
 # @Software: PyCharm
 # @define  : function
-
-# import pandas as pd
-# import numpy as np
-# import tensorflow as tf
-# import matplotlib.pyplot as plt
+import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Dropout
+from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D, Dropout, MaxPool2D
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras import backend as K
 import json
 
-batch_size = 256
+
+batch_size = 128
 num_classes = 10
-epochs = 100
+epochs = 10
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -47,27 +44,33 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 print(input_shape)
+
+# 构建网络
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+# 第一个卷积层，32个卷积核，大小５x5，卷积模式SAME,激活函数relu,输入张量的大小
+model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu',
+                 input_shape=(28, 28, 1)))
+model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu'))
+# 池化层,池化核大小２x2
+model.add(MaxPool2D(pool_size=(2, 2)))
+# 随机丢弃四分之一的网络连接，防止过拟合
 model.add(Dropout(0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.25))
+# 全连接层,展开操作，
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+# 添加隐藏层神经元的数量和激活函数
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.25))
+# 输出层
+model.add(Dense(10, activation='softmax'))
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['acc'])
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
+model.fit(x_train, y_train, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
