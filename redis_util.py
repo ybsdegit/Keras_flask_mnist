fe94efa19bd5a1a8b276fcd6941aa8e96e1865a7
ybsdegit <ybsdeyx@163.com>
2020-04-25 23:16:27 +0800

    redis 修改

--- redis_util.py
+++ redis_util.py
@@ -20,16 +20,27 @@ TODAY_KEY = "TODAY"  # 今日日期
 TODAY_TIME_KEY = "TODAY_TIME"  # 今日访问次数
 
 # 初始化redis
-r = redis.StrictRedis(host=REDIS_HOST)
-
+try:
+    r = redis.StrictRedis(host=REDIS_HOST)
+except:
+    print("请查看redis是否配置")
 # 当前访问次数 查看日志中请求的次数，设置初始值  wc -l nohup.out
 # 8390
 # r.set(MINIST_KEY, 8390)
 # r.set(TODAY_TIME_KEY, 1)
 # 从redis中获取当前的访问次数
-r.get(MINIST_KEY)
+
 
 def inc_visit_num():
+    count = int(r.get(MINIST_KEY).decode())
+    if count < 100:
+        print("redis中的访问量有问题，读取日志重新设置值")
+        try:
+            count = cal_line_num()
+            r.set(MINIST_KEY, count)
+        except:
+            print("不是生产环境，请忽略")
+        
     r.incr(MINIST_KEY)
     if (get_today().encode() == r.get(TODAY_KEY)):
         r.incr(TODAY_TIME_KEY)
@@ -47,7 +58,13 @@ def get_visit_num_today():
 def get_today():
     return date.today().strftime('%Y-%m-%d')
 
-
+def cal_line_num():
+    i = 0
+    with open("nohup.out", 'r', encoding='utf-8') as f:
+        for i, line in enumerate(f, start=1):
+            i = i+1
+    print("LOG  总行数量: ", i)
+    return i
 
 if __name__ == '__main__':
     inc_visit_num() # 访问一次
